f9055cc719b4d80a7430d27f1b9d65238341ccec
Sunday Moses <moseschizaram8@gmail.com>
2023-04-03 20:42:29 +0100

    mend

--- words.py
+++ words.py
@@ -1,10 +1,2 @@
-great = \
-    '''
-Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum magni laudantium ab, assumenda officiis explicabo saepe iste voluptatem mollitia ipsa, a optio ipsam sapiente error vitae voluptas quo id, repellat quam esse? Quidem distinctio ad, animi esse illum saepe, facilis repellat porro dolorum itaque optio molestiae debitis obcaecati dignissimos exercitationem!
-'''
-
-# words = ['glory', 'wonderful', 'age', 'father', 'mother',
-#          'children', 'football', 'tennis', 'microwave']
-
 another_word = ["awesome", "grace", "church", "basic", "primary", "university", "national", "Beautiful", "ugliness", "handsome",
                 "morning", "rose", "bouncing", "grateful", "appreciate", "loneliness", "music", "tutorial", "dictionary", "computer", "technology", "information"]
